b1f43211ced21da8989de9ed0eb1fca64848ef54
tabish.khan <tabish.khan@tothenew.com>
2016-08-27 12:56:39 +0530

    Added first hello world react program

---
+++ src/component/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//const{component} = 'react'
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        This is app
+    </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
  * Created by ttnd on 27/8/16.
  */
 console.log("Hello KMT");
+
+import React from 'react';
+
+import {render} from 'react-dom';
+
+import App from './component/app'
+
+
+render(
+  <App />,
+    document.getElementById("app")
+
+)
